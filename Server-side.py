dfef5e981edb4124d47abf17c6d3160d26d54403
Sturdystablestalwart <72163215+sturdystablestalwart@users.noreply.github.com>
2020-10-02 06:33:41 +0300

    Server shows html page with a text message

--- Server-side.py
+++ Server-side.py
@@ -1,2 +1,9 @@
 # Хотел начать сегодня, но с ног валюсь - _ - 01.10.20
 # С утреца пару строк осилю :3
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return "Suck me daddy"
