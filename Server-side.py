d5e8e29ce5c31b4abeaa3489f2654ae551e548e9
Reznitskiy <alexander.reznitskiy@effem.com>
2020-10-03 00:17:32 +0300

    Modified server to respond to POST methods

--- Server-side.py
+++ Server-side.py
@@ -1,9 +1,13 @@
 # Хотел начать сегодня, но с ног валюсь - _ - 01.10.20
 # С утреца пару строк осилю :3
-from flask import Flask, request, jsonify
-
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Suck me daddy"
+	return 'Suck me daddy!'
+
+@app.route('/The thing', methods=['POST'])
+def The_thing():
+	if request.method == 'POST':
+		return 'Suck me daddy'
