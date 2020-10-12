8946fa2e46b0000ae3a17502b2985b8f5669427a
sturdystablestalwart <laosen@mail.ru>
2020-10-12 22:58:14 +0300

    Did PATCH part, tryed adding select by id and field name -- didn't work

--- request_handling.py
+++ request_handling.py
@@ -5,6 +5,7 @@ from flask import request, make_response
 from insert_in_table import insert_new_quask
 import time
 from select_from_db import *
+from update_field_in_table import update_is_done_quask
 
 
 def handle_request_post():
@@ -39,19 +40,40 @@ def handle_request_get_id(quask_num):
 		return dumps(line_from_table, indent=1), 200, {'Content-Type': 'application/json'}
 
 
+def handle_request_patch_id(quask_num):
+	print(int(bool(request.get_json()['is_done'])))
+	print(select_quask_by_id(quask_num)[3])
+	if str(select_quask_by_id(quask_num)) == 'None':
+		return '', 404
+	elif request.content_type != 'application/json':
+		return '', 400
+	elif 'is_done' not in request.get_json():
+		return '', 400
+	elif int(bool(request.get_json()['is_done'])) == select_quask_by_id(quask_num)[3]:
+		return '', 204
+	elif int(bool(request.get_json()['is_done'])) != select_quask_by_id(quask_num)[3]:
+		table_update = update_is_done_quask(int(bool(request.get_json()['is_done'])), quask_num)
+
+		return table_update, 200
+	select_quask_by_id(quask_num)
+	data_from_post = request.get_json()['is_done']
+	return
+
+
 def create_post_response():
 	response = make_response(handle_request_post())
-
 	return response
 
 
 def create_get_response():
 	response = make_response(handle_request_get())
-
 	return response
 
 
 def create_get_id_response(quask_num):
 	response = make_response(handle_request_get_id(quask_num))
+	return response
 
+def create_patch_id_response(quask_num):
+	response=make_response(handle_request_patch_id(quask_num))
 	return response
