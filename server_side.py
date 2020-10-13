4aad37ab7f85d087b05d1b37dd525c3fabd94cf8
sturdystablestalwart <laosen@mail.ru>
2020-10-13 23:44:21 +0300

    I did VERY basic Basic auth... it works tho

--- server_side.py
+++ server_side.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from request_handling import create_post_response, create_get_response, create_get_id_response, create_patch_id_response
 from create_db import create_db
 
@@ -9,7 +9,7 @@ create_db()
 
 @app.route('/')
 def index():
-	return 'Wrong way Bud!', 404
+	return 'Wrong way Bud!'
 
 
 @app.route('/tasks', methods=['POST'])
@@ -32,6 +32,15 @@ def respond_to_patch_id(quask_num):
 	return create_patch_id_response(quask_num)
 
 
+@app.route('/account/login')
+def login_user():
+	if request.authorization and request.authorization.username == "Sanya" \
+							and request.authorization.password == "Kolya":
+		return make_response("Logged", 200)
+	else:
+		return make_response('Nope', 401,{'WWW-Authenticate': 'Basic realm="Login required"'})
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
@@ -46,4 +55,6 @@ GETforWin
 -X GET http://localhost:5000/tasks
 PATCHforWin
 -X PATCH http://localhost:5000/tasks/1 -i -H "Content-Type: application/json" -d "{\"is_done\":\"1\"}"
+LOGINforWIN
+curl -u Sanya:Kolya http://localhost:5000/account/login
 '''
