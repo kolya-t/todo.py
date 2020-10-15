1c4b43868e4ce3f5e9efcea0c6d9610f068d01c5
sturdystablestalwart <laosen@mail.ru>
2020-10-15 16:52:11 +0300

    did pip freez

--- loging.py
+++
@@ -1,7 +0,0 @@
-import flask
-if not server_side.debug:
-    import logging
-    from themodule import TheHandlerYouWant
-    file_handler = TheHandlerYouWant(...)
-    file_handler.setLevel(logging.WARNING)
-    app.logger.addHandler(file_handler)
